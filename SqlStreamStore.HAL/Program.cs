41090431fdd37f4363d4ef05cd6964de3bafb574
Mat McLoughlin <mathew.mcloughlin@gmail.com>
2016-10-29 21:14:57 +0100

    starting

---
+++ SqlStreamStore.HAL/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Owin.Builder;
+using Microsoft.Owin.Hosting;
+using Owin;
+using SqlStreamStore.Streams;
+using MidFunc = System.Func<
+    System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>,
+    System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>>;
+
+namespace SqlStreamStore.HAL
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var streamStore = new InMemoryStreamStore();
+            var messages = SeedData.Get(1000);
+
+            streamStore.AppendToStream("SomeStream", ExpectedVersion.Any, messages);
+
+            var settings = new HALSettings {Url = "Stream"};
+            var baseUrl = "http://localhost:8080";
+
+            using (WebApp.Start(baseUrl, app => app.Use(HALMiddleware.Handle(settings))))
+            {
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class FooMessage
+    {
+    }
+
+    internal static class HALMiddleware
+    {
+        public static MidFunc Handle(HALSettings settings)
+        {
+            return next =>
+            {
+                var appBuilder = new AppBuilder();
+                appBuilder.Run(context =>
+                {
+                    context.Response.ContentType = "text/plain";
+
+                    string output = string.Format(
+                        "I'm running on {0} nFrom assembly {1}",
+                        Environment.OSVersion,
+                        System.Reflection.Assembly.GetEntryAssembly().FullName
+                    );
+
+                    return context.Response.WriteAsync(output);
+                });
+                return appBuilder.Build();
+            };
+        }
+    }
+
+    internal class HALSettings
+    {
+        public string Url { get; set; }
+    }
+}
---
+++ SqlStreamStore.HAL/SeedData.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using SqlStreamStore.Streams;
+using StreamStoreStore.Json;
+
+namespace SqlStreamStore.HAL
+{
+    public static class SeedData
+    {
+        static readonly Dictionary<int, Func<object>> Factory = new Dictionary<int, Func<object>>
+        {
+            {0, Fizz.Get},
+            {1, FizzBuzz.Get},
+            {2, Buzz.Get}
+        };
+
+        public static IEnumerable<NewStreamMessage> Get(int count)
+        {
+            var rand = new Random();
+
+            for (var i = 0; i < count; i++)
+            {
+                var random = rand.Next(0, 3);
+                var message = Factory[random]();
+
+                yield return new NewStreamMessage(
+                    Guid.NewGuid(),
+                    message.GetType().FullName.ToString(),
+                    SimpleJson.SerializeObject(new FooMessage())
+                );
+            }
+        }
+    }
+
+    public class Fizz
+    {
+        public int Lorem { get; set; }
+
+        public int Ipsum { get; set; }
+
+        public int Dolor { get; set; }
+
+        public static Fizz Get()
+        {
+            var rand = new Random();
+            return new Fizz
+            {
+                Lorem = rand.Next(0, 10000000),
+                Ipsum = rand.Next(0, 10000),
+                Dolor = rand.Next(0, 100)
+            };
+        }
+    }
+
+    public class Buzz
+    {
+        public int Consectetur { get; set; }
+
+        public int Adipiscing { get; set; }
+
+        public int Aliquam { get; set; }
+
+        public static Buzz Get()
+        {
+            var rand = new Random();
+            return new Buzz
+            {
+                Consectetur = rand.Next(0, 10000000),
+                Adipiscing = rand.Next(0, 10000),
+                Aliquam = rand.Next(0, 100)
+            };
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public int Hendrerit { get; set; }
+
+        public int Efficitur { get; set; }
+
+        public int Libero { get; set; }
+
+        public static FizzBuzz Get()
+        {
+            var rand = new Random();
+            return new FizzBuzz
+            {
+                Hendrerit = rand.Next(0, 10000000),
+                Efficitur = rand.Next(0, 10000),
+                Libero = rand.Next(0, 100)
+            };
+        }
+    }
+}
