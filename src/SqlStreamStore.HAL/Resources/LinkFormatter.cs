8fc61e9362eb60cb7a436e1c2ed24e025b26c211
João Bragança <joao.paul@braganca.name>
2018-05-30 12:49:37 +0200

    doesn't need to be a number

--- src/SqlStreamStore.HAL/Resources/LinkFormatter.cs
+++ src/SqlStreamStore.HAL/Resources/LinkFormatter.cs
@@ -3,7 +3,7 @@ namespace SqlStreamStore.HAL.Resources
     internal static class LinkFormatter
     {
         private static string FormatLink(string baseAddress, string direction, int maxCount, long position, bool prefetch)
-            => $"{baseAddress}?d={direction}&m={maxCount}&p={position}{(prefetch ? "&e=1" : string.Empty)}";
+            => $"{baseAddress}?d={direction}&m={maxCount}&p={position}{(prefetch ? "&e" : string.Empty)}";
 
         public static string FormatForwardLink(string baseAddress, int maxCount, long position, bool prefetch)
             => FormatLink(baseAddress, "f", maxCount, position, prefetch);
