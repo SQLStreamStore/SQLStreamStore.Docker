d825f562d37efc92e2357f8f33232cad676979ee
João Bragança <joao.paul@braganca.name>
2018-06-29 15:32:43 +0200

    delete shouldn't have a response body

--- src/SqlStreamStore.HAL/HttpContextExtensions.cs
+++ src/SqlStreamStore.HAL/HttpContextExtensions.cs
@@ -31,6 +31,11 @@ namespace SqlStreamStore.HAL
                 context.Response.Headers.Append(header.Key, header.Value);
             }
 
+            if(context.IsDelete())
+            {
+                return;
+            }
+
             using(var writer = new JsonTextWriter(new StreamWriter(context.Response.Body))
             {
                 CloseOutput = false
