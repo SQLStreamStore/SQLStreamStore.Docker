6226d9dd4c238613fb450e8bed969d4cd3732d52
João Bragança <joao.paul@braganca.name>
2018-07-03 15:40:51 +0200

    javascript ui now uses absolute urls so simply forward

--- src/SqlStreamStore.HAL.DevServer/Program.cs
+++ src/SqlStreamStore.HAL.DevServer/Program.cs
@@ -1,7 +1,5 @@
 ﻿namespace SqlStreamStore.HAL.DevServer
 {
-    using System;
-    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Hosting;
