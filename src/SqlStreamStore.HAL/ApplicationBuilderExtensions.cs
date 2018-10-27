33e6b7e14e96a8c5234cb668eaaf6490c78d8b08
thefringeninja <joao.paul@braganca.name>
2018-10-27 16:51:34 +1000

    moved allowed methods into own middleware

--- src/SqlStreamStore.HAL/ApplicationBuilderExtensions.cs
+++ src/SqlStreamStore.HAL/ApplicationBuilderExtensions.cs
@@ -1,17 +1,8 @@
 namespace SqlStreamStore.HAL
 {
     using System;
-    using System.Linq;
     using System.Net.Http;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.Extensions.Primitives;
-    using MidFunc = System.Func<
-        Microsoft.AspNetCore.Http.HttpContext,
-        System.Func<System.Threading.Tasks.Task>,
-        System.Threading.Tasks.Task
-    >;
 
     internal static class ApplicationBuilderExtensions
     {
@@ -24,52 +15,5 @@ namespace SqlStreamStore.HAL
                 method.Method,
                 StringComparison.OrdinalIgnoreCase),
             configure);
-
-        public static IApplicationBuilder UseAllowedMethods(this IApplicationBuilder builder, IResource resource)
-        {
-            var allowed = ResourceMethods.Discover(resource);
-
-            var allowedMethodsHeaderValue = allowed.Aggregate(
-                StringValues.Empty,
-                (previous, method) => StringValues.Concat(previous, method.Method));
-
-            var allowedHeadersHeaderValue = new StringValues(new[]
-            {
-                Constants.Headers.ContentType,
-                Constants.Headers.XRequestedWith,
-                Constants.Headers.Authorization
-            });
-            
-            Task Options(HttpContext context, Func<Task> next)
-            {
-                context.Response.Headers.AppendCommaSeparatedValues(
-                    Constants.Headers.AccessControl.AllowMethods,
-                    allowedMethodsHeaderValue);
-                context.Response.Headers.AppendCommaSeparatedValues(
-                    Constants.Headers.AccessControl.AllowHeaders,
-                    allowedHeadersHeaderValue);
-                context.Response.Headers.AppendCommaSeparatedValues(
-                    Constants.Headers.AccessControl.AllowOrigin,
-                    "*");
-                
-                return Task.CompletedTask;
-            }
-
-            Task AllowedMethods(HttpContext context, Func<Task> next)
-            {
-                if(!allowed.Contains(new HttpMethod(context.Request.Method)))
-                {
-                    context.Response.StatusCode = 405;
-                    context.Response.Headers.Add(Constants.Headers.Allowed, allowedMethodsHeaderValue);
-                    return Task.CompletedTask;
-                }
-
-                return next();
-            }
-
-            return builder
-                .MapWhen(HttpMethod.Options, inner => inner.Use(Options))
-                .Use(AllowedMethods);
-        }
     }
 }
