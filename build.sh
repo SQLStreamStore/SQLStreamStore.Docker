57d47f8ecab5c24f3f982e0b6978e1b8310b1fc6
thefringeninja <joao.paul@braganca.name>
2018-12-02 10:51:06 +0100

    pushing to docker

--- build.sh
+++ build.sh
@@ -37,4 +37,9 @@ else
     docker tag $LOCAL $MAJOR_MINOR_PATCH_PRE
 fi
 
+if [[ -n $DOCKER_USER ]]; then
+    echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
+    docker push $REMOTE_IMAGE
+fi
+
 docker images --filter=reference="${REMOTE_IMAGE}"
