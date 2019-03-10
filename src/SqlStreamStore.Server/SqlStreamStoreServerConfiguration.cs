7d90ad09cd9679279bddc16b9792931c0af30e95
thefringeninja <495495+thefringeninja@users.noreply.github.com>
2019-03-10 14:28:24 +0100

    use nameof intead of other stuff for keys

--- src/SqlStreamStore.Server/SqlStreamStoreServerConfiguration.cs
+++ src/SqlStreamStore.Server/SqlStreamStoreServerConfiguration.cs
@@ -12,14 +12,14 @@ namespace SqlStreamStore.Server
 {
     internal class SqlStreamStoreServerConfiguration
     {
-        private readonly IConfigurationRoot _configuration;
+        private readonly ConfigurationData _configuration;
         private Dictionary<string, (string source, string value)> _values;
 
-        public bool UseCanonicalUris => _configuration.GetValue<bool>("use-canonical-uris");
-        public LogEventLevel LogLevel => _configuration.GetValue("log-level", LogEventLevel.Information);
-        public string ConnectionString => _configuration.GetValue<string>("connection-string");
-        public string Schema => _configuration.GetValue<string>("schema");
-        public string Provider => _configuration.GetValue<string>("provider");
+        public bool UseCanonicalUris => _configuration.UseCanonicalUris;
+        public LogEventLevel LogLevel => _configuration.LogLevel;
+        public string ConnectionString => _configuration.ConnectionString;
+        public string Schema => _configuration.Schema;
+        public string Provider => _configuration.Provider;
 
         public SqlStreamStoreServerConfiguration(
             IDictionary environment,
@@ -37,40 +37,96 @@ namespace SqlStreamStore.Server
                 foreach (var (key, value) in data)
                 {
                     _values[key] = (logName, value);
-                    Console.WriteLine($"{key} came from {logName}");
                 }
             }
 
-            _configuration = new ConfigurationBuilder()
-                .Add(new CommandLineConfigurationSource(args, Log))
-                .Add(new EnvironmentVariablesConfigurationSource(environment, Log))
-                .Build();
+            _configuration = new ConfigurationData(
+                new ConfigurationBuilder()
+                    .Add(new DefaultConfigurationSource(Log))
+                    .Add(new CommandLineConfigurationSource(args, Log))
+                    .Add(new EnvironmentVariablesConfigurationSource(environment, Log))
+                    .Build());
         }
 
         public override string ToString()
         {
             const string delimiter = " | ";
 
-            var column0Width = _values.Keys.Count > 0 ? _values.Keys.Max(x => x.Length) : 0;
-            var column1Width = _values.Values.Count > 0 ?_values.Values.Max(_ => _.value.Length) : 0;
-            var column2Witdth = _values.Values.Count > 0 ?_values.Values.Max(_ => _.source.Length) : 0;
+            var column0Width = _values.Keys.Count > 0 ? _values.Keys.Max(x => x?.Length ?? 0) : 0;
+            var column1Width = _values.Values.Count > 0 ? _values.Values.Max(_ => _.value?.Length ?? 0) : 0;
+            var column2Width = _values.Values.Count > 0 ? _values.Values.Max(_ => _.source?.Length ?? 0) : 0;
 
             StringBuilder Append(StringBuilder builder, string column0, string column1, string column2) =>
                 builder
-                    .Append(column0.PadRight(column0Width, ' '))
+                    .Append((column0 ?? string.Empty).PadRight(column0Width, ' '))
                     .Append(delimiter)
-                    .Append(column1.PadRight(column1Width, ' '))
+                    .Append((column1 ?? string.Empty).PadRight(column1Width, ' '))
                     .Append(delimiter)
                     .AppendLine(column2);
 
             return _values.Keys.OrderBy(x => x).Aggregate(
                 Append(new StringBuilder().AppendLine("SQL Stream Store Configuration:"), "Argument", "Value", "Source")
-                    .AppendLine(new string('-', column0Width + column1Width + column2Witdth)),
+                    .AppendLine(new string('-', column0Width + column1Width + column2Width)),
                 (builder, key) => Append(
                     builder,
                     key,
                     _values[key].value,
                     _values[key].source)).ToString();
+        }
+
+        private static string Computerize(string value) =>
+            string.Join(
+                string.Empty,
+                (value?.Replace("-", "_").ToLowerInvariant()
+                 ?? string.Empty).Split('_')
+                .Select(x => new string(x.Select((c, i) => i == 0 ? char.ToUpper(c) : c).ToArray())));
+
+        private class ConfigurationData
+        {
+            private readonly IConfigurationRoot _configuration;
+
+            public bool UseCanonicalUris => _configuration.GetValue<bool>(nameof(UseCanonicalUris));
+            public LogEventLevel LogLevel => _configuration.GetValue(nameof(LogLevel), LogEventLevel.Information);
+            public string ConnectionString => _configuration.GetValue<string>(nameof(ConnectionString));
+            public string Schema => _configuration.GetValue<string>(nameof(Schema));
+            public string Provider => _configuration.GetValue<string>(nameof(Provider));
+
+            public ConfigurationData(IConfigurationRoot configuration)
+            {
+                if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+                _configuration = configuration;
+            }
+        }
+
+        private class DefaultConfigurationSource : ConfigurationProvider, IConfigurationSource
+        {
+            private readonly Action<string, IDictionary<string, string>> _log;
+
+            public DefaultConfigurationSource(Action<string, IDictionary<string, string>> log)
+            {
+                if (log == null)
+                {
+                    throw new ArgumentNullException(nameof(log));
+                }
+
+                _log = log;
+            }
+
+            public IConfigurationProvider Build(IConfigurationBuilder builder) => this;
+
+            public override void Load()
+            {
+                Data = new Dictionary<string, string>
+                {
+                    [nameof(ConnectionString)] = default,
+                    [nameof(Provider)] = "inmemory",
+                    [nameof(LogLevel)] = nameof(LogEventLevel.Information),
+                    [nameof(Schema)] = default,
+                    [nameof(UseCanonicalUris)] = default
+                };
+
+                _log(nameof(DefaultConfigurationSource), Data);
+            }
         }
 
         private class CommandLineConfigurationSource : IConfigurationSource
@@ -120,6 +176,8 @@ namespace SqlStreamStore.Server
             public override void Load()
             {
                 base.Load();
+
+                Data = Data.Keys.ToDictionary(Computerize, x => Data[x]);
 
                 _log(nameof(CommandLineConfigurationSource), Data);
             }
@@ -186,7 +244,7 @@ namespace SqlStreamStore.Server
                         where key.StartsWith(_prefix)
                         select new
                         {
-                            key = key.Remove(0, _prefix.Length).Replace('_', '-').ToLowerInvariant(),
+                            key = Computerize(key.Remove(0, _prefix.Length)),
                             value = (string) entry.Value
                         })
                     .ToDictionary(x => x.key, x => x.value);
